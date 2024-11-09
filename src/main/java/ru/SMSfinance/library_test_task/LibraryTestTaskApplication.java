e1d043e7b0fac63682560f8aecb0cd0ee75efa78
Sergey Shadrin <juuute3110@gmail.com>
2024-11-09 20:00:35 +0700

    first commit

---
+++ src/main/java/ru/SMSfinance/library_test_task/LibraryTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.SMSfinance.library_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/SMSfinance/library_test_task/LibraryTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.SMSfinance.library_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
